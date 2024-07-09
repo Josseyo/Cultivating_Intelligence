c5740ff7a7db38c7513b77ca4f055e24a56111c7
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-07-09 10:29:05 +0000

    Add base.html, index.html, static css. Add browse post view.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Post, Comment
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = "index.html"
+    paginate_by = 6
--- cultivating_intelligence/settings.py
+++ cultivating_intelligence/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-josseyo-cultivatinginte-g2rcff6illk.ws.codeinstitute-ide.net','cultivating-intelligence-1ead7384db49.herokuapp.com', 'localhost',]
 
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE ='cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
