196cb91110efd652a17ed8190b8bd0578efddd6b
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-07-22 18:07:23 +0000

    Change the "updated_on" field in the Comment model to auto_now=True.

--- blog/models.py
+++ blog/models.py
@@ -106,7 +106,7 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
    
