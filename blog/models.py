5cfbf7d0809325c7c87ea7b44126b187f108517d
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-08-14 17:35:26 +0000

    - Restored the `updated_on` field in the Comment model for tracking updates. - Removed commented-out fields (name and email)

--- blog/models.py
+++ blog/models.py
@@ -79,7 +79,7 @@ class Comment(models.Model):
     #email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    #updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
