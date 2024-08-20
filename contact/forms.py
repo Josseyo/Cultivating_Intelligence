e64d0815f9101b1519b69172fd19b10306d5a9fc
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-08-20 16:02:48 +0000

    Add Contact form.py with collaboration form
    Add url path to contact view in url.py

---
+++ contact/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import CollaborateRequest
+
+
+class CollaborateForm(forms.ModelForm):
+  
+    class Meta:
+        """
+        Specify the django model and order of the fields
+        """
+        model = CollaborateRequest
+        fields = ('name', 'email', 'message')
