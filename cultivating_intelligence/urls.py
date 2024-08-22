c2a77d141ad6887cbb1ba48c2a279c627f0b306e
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-08-22 10:11:17 +0000

    Add contact path to project urlpatterns

--- cultivating_intelligence/urls.py
+++ cultivating_intelligence/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
+    path('', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
+    path('contact/', include('contact.urls'), name='contact-urls'), 
 ]
