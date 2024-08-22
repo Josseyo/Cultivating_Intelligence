74c2b65600b59e6dfa576a17de12d4772373f5c2
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-08-22 10:07:50 +0000

    Add collaboration request management in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact, CollaborateRequest
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    """Admin panel for CollaborateRequestAdmin"""
+    list_display = ('name', 'email', 'read', 'message',)
+    list_filter = ('read', 'name', 'email',)
+    search_fields = ['name', 'email', 'read', 'message',]
