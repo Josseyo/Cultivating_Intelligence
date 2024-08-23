8e1bf034e66b4ad94a8049e07ddc12754fa11aaf
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-08-23 19:01:23 +0000

    Refactor comment functionality
    - Wrapp event listener initialization in DOMContentLoaded.
    - Move modal initialization into DOMContentLoaded.
    - Comment out console log for cleaner production code.

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,31 +1,33 @@
-const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_body");
-const commentForm = document.getElementById("commentForm");
-const submitButton = document.getElementById("submitButton");
+document.addEventListener("DOMContentLoaded", function() {
+    const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+    
+    const editButtons = document.getElementsByClassName("btn-edit");
+    const commentText = document.getElementById("id_body");
+    const commentForm = document.getElementById("commentForm");
+    const submitButton = document.getElementById("submitButton");
+    const deleteButtons = document.getElementsByClassName("btn-delete");
+    const deleteConfirm = document.getElementById("deleteConfirm");
 
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
-const deleteConfirm = document.getElementById("deleteConfirm");
+    // Add event listeners for edit buttons
+    Array.from(editButtons).forEach(button => {
+        button.addEventListener("click", (e) => {
+            const commentId = e.target.getAttribute("data-comment_id");
+            const commentContent = document.getElementById(`comment${commentId}`).innerText;
 
-// Add event listeners for edit buttons
-Array.from(editButtons).forEach(button => {
-    button.addEventListener("click", (e) => {
-        const commentId = e.target.getAttribute("data-comment_id");
-        const commentContent = document.getElementById(`comment${commentId}`).innerText;
-        
-        commentText.value = commentContent;
-        submitButton.innerText = "Update";
-        commentForm.setAttribute("action", `edit_comment/${commentId}`);
-        
-        console.log(`edit_comment/${commentId}`);
+            commentText.value = commentContent;
+            submitButton.innerText = "Update";
+            commentForm.setAttribute("action", `edit_comment/${commentId}`);
+
+            /*console.log(`Editing comment ID: ${commentId}`);*/
+        });
     });
-});
 
-// Add event listeners for delete buttons
-Array.from(deleteButtons).forEach(button => {
-    button.addEventListener("click", (e) => {
-        const commentId = e.target.getAttribute("data-comment_id");
-        deleteConfirm.href = `delete_comment/${commentId}`;
-        deleteModal.show();
+    // Add event listeners for delete buttons
+    Array.from(deleteButtons).forEach(button => {
+        button.addEventListener("click", (e) => {
+            const commentId = e.target.getAttribute("data-comment_id");
+            deleteConfirm.href = `delete_comment/${commentId}`;
+            deleteModal.show();
+        });
     });
 });
