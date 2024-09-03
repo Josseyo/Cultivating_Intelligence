5c648d678a23ef41a2293a1eaebcd079b65677d8
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-09-03 11:49:57 +0000

    Format code and docstrings

--- blog/models.py
+++ blog/models.py
@@ -3,80 +3,109 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils.text import slugify
 
-# Status for Post model
-STATUS = (
-    (0, 'Draft'),
-    (1, 'Publish')
-)
+"""
+Status for Post model
+"""
+STATUS = ((0, "Draft"), (1, "Publish"))
+
 
 class Category(models.Model):
+    """
+    Model for blog category.
+
+    Attributes:
+        name (str): The name of the category.
+    """
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+        Returns the name of the category as a string.
+        """
         return self.name
-""" 
-Post Model,
-to create and manage blog posts:
-    Title - post title
-    Slug - to store and generate valid URL path automatically
-    Author - post author, assigned by username automatically
-    Category_type - category type such as category 1 & category 2, used for post filter
-    Media_type - media type such as text, video & pod, used for post filter
-    Excerpt - post description  
-    Body - post content
-    Image - post image, if none provided, a default image is displayed
-    Created_on - post creation date, used to organise recipes in order
-    Updated_on - post update date, used for admin purposes
-    Status - post status, draft or published
-    Likes - number of likes by users
-"""
+
+
 class Post(models.Model):
+    """
+     Post model to create and managing blog post:
+
+    Attributes:
+         title (str): Title of the post.
+         category (Category): The category the post belongs to.
+         slug (str): A URL-friendly version of the title.
+         author (User): Author of the post.
+         featured_image (CloudinaryField): Image representing the post.
+         excerpt (str): Short summary of the post.
+         content (str): Main content of the post.
+         created_on (datetime): Date and time the post was created.
+         updated_on (datetime): Date and time the post was last updated.
+         status (int): Publication status of the post (Draft or Publish).
+         likes (ManyToManyField): Users who liked the post.
+    """
+
     title = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    featured_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField("image", default="placeholder")
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name="blogpost_like", blank=True
+    )
 
     class Meta:
         """
-        Ordering of posts by their creation dates in descending order
+        Meta class for Post model for ordering the posts
+
+        Attributes:
+            ordering (list): Specifies the default ordering of posts
+        by creation date in descending order.
         """
-        ordering = ['-created_on']
+
+        ordering = ["-created_on"]
 
     def __str__(self):
-        """Returns post title as a string"""
+        """Returns the title of the post as a string."""
         return self.title
 
     def number_of_likes(self):
-        """Returns total number of likes for each post"""
-        return self.likes.count() 
+        """Returns the total number of likes for each post."""
+        return self.likes.count()
 
     def save(self, *args, **kwargs):
+        """Overrides default save method to auto-generate slug from title."""
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
-""" 
-Comment Model,
-to create and manage comments:
-    Auther - comment author, assigned by username automatically
-    Email - user email, assigned by user email automatically
-    Content - comment content
-    Created_on - comment creation date, used to organise comments in order
-    Updated_on - comment update date, used for admin purposes
-"""
-class Comment(models.Model):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-    #name = models.CharField(max_length=80) 
-    #email = models.EmailField()
+class Comment(models.Model):
+    """
+    Comment model for creating and managing comments on blog posts.
+
+     Attributes:
+         post (Post): The post to which the comment belongs.
+         author (User): Author of the comment.
+         body (str): Content of the comment.
+         created_on (datetime): Date and time the comment was created.
+         updated_on (datetime): Date and time the comment was last updated.
+         approved (bool): Indicates if comment is approved for publication,
+         default is set to True.
+    """
+
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -84,12 +113,15 @@ class Comment(models.Model):
 
     class Meta:
         """
-        Order comments by their creation dates in ascending order
-        """    
-        ordering = ['created_on']
+        Meta options for Comment model.
+
+        Attributes:
+            ordering (list): Default ordering of comments by creation
+            date in ascending order.
+        """
+
+        ordering = ["created_on"]
 
     def __str__(self):
-        """Returns comment body and author"""    
+        """Returns string of the comment including body and author."""
         return f"Comment {self.body} by {self.author}"
-
-
